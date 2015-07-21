78627d3227d2ce7a60e6989fc8b54326949cf9c9
ffmm <masamail4210@gmail.com>
2015-07-21 18:27:36 +0900

    加速度の取得

---
+++ app/src/main/java/com/fm_example/acceleget/MainActivity.java
@@ -0,0 +1,84 @@
+package com.fm_example.acceleget;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+
+    public void onSensorChanged(SensorEvent event) {
+        StringBuilder strBuild = new StringBuilder();
+
+        strBuild.append("X軸");
+        strBuild.append(event.values[0]);
+        strBuild.append("\n");
+        strBuild.append("Y軸");
+        strBuild.append(event.values[1]);
+        strBuild.append("\n");
+        strBuild.append("Z軸");
+        strBuild.append(event.values[2]);
+        strBuild.append("\n");
+        TextView text01 = (TextView) findViewById(R.id.text01);
+        text01.setText(strBuild.toString());
+
+    }
+
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
